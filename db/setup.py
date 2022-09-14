61e09dc249e8085e6770b86c0fe5b794ad2252d9
goosegusgo <goosegusgo@proton.me>
2022-09-14 19:01:00 +0300

    base fuctional

--- db/setup.py
+++
@@ -1,8 +0,0 @@
-from create_db import DatBase
-from config import database, user, password, host, port, name_database
-
-if __name__ == "__main__":
-    db = DatBase(database, user, password, host, port, name_database)
-    db.create_db()
-    db.create_tables()
-    print("Setup database successfully")
